0abfe3768ee14d180902d25ab724d853277d6a5f
Dimetriu <dima.rashidov@gmail.com>
2019-07-21 13:47:54 +0300

    add constants to app

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const todoForm = document.getElementById("tot-form");
+const addINput = document.getElementById("add-input");
+const todoList = document.getElementById("toto-list");
+const todoItems = document.querySelectorAll(".todo-item");
