cb433400b0ddb3f4ef60f46f96c9af60f287897e
Dimetriu <dima.rashidov@gmail.com>
2019-07-21 21:19:59 +0300

    implement todo item adding

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-function addTodoItem(e) {
-  e.preventDefault();
-
-  if (addInput.value === "") return alert(
-    "Необходимо ввести название задачи."
-  );
-
-  const listItem = createTodoItem(addInput.value);
-}
-
 function createTodoItem(title) {
   const checkbox = document.createElement("input");
 
@@ -22,9 +12,9 @@ function createTodoItem(title) {
   editInput.type = "text";
   editInput.className = "textfield";
 
-   const editButton = document.createElement("button");
-   editButton.innerText = "Изменить";
-   editButton.className = "edit";
+  const editButton = document.createElement("button");
+  editButton.innerText = "Изменить";
+  editButton.className = "edit";
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "Удалить";
@@ -41,9 +31,21 @@ function createTodoItem(title) {
   return listItem;
 }
 
-const todoForm = document.getElementById("tot-form");
+function addTodoItem(e) {
+  e.preventDefault();
+
+  if (addInput.value === "") return alert(
+    "Необходимо ввести название задачи."
+  );
+
+  const todoItem = createTodoItem(addInput.value);
+  todoList.appendChild(todoItem);
+  addInput.value = "";
+}
+
+const todoForm = document.getElementById("todo-form");
 const addInput = document.getElementById("add-input");
-const todoList = document.getElementById("toto-list");
+const todoList = document.getElementById("todo-list");
 const todoItems = document.querySelectorAll(".todo-item");
 
 todoForm.addEventListener("submit", addTodoItem);
